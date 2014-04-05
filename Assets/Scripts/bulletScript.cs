2f1d0fc3f950662334526c04e24f9a8e11a6a310
Dylan Hughes <dylanbhughes@gmail.com>
2014-04-05 17:34:11 -0400

    Initial Commit of Dylan's Prototype
    This is the code for my prototype 3 that I submitted for class.

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+	
+	public float speed = 40f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Bullets move forward at a constant rate
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		//If the thing we just collided with is 'damagable' (a tag I created and applied to the wall), assume 
+		//we ran into the wall and make its health smaller.
+		if (other.gameObject.tag != "Player") {
+			if (other.gameObject.tag == "takesDamage") {
+				minionScript mi = other.gameObject.GetComponent<minionScript> ();
+				mi.currentHealth -= 1;
+				
+				//If the wall's health is 0, destroy it.
+				if (mi.currentHealth <= 0) {
+					Destroy (other.gameObject);
+				}
+			}
+			else if(other.gameObject.tag == "tower"){
+				towerScript tw = other.gameObject.GetComponent<towerScript> ();
+				tw.currentHealth -= 1;
+				
+				//If the tower's health is 0, destroy it.
+				if (tw.currentHealth < 0) {
+					Destroy (other.gameObject);
+				}
+			}
+			Destroy (gameObject);
+		}
+	}
+}
