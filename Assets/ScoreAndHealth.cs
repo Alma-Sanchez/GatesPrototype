0d31bb97f8bc2bb1a5b5ae69f41869a1bc5c86dc
Alma Sanchez <as0820a@american.edu>
2014-04-16 18:43:02 -0400

    Add gitignore, remove junk

---
+++ Assets/ScoreAndHealth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAndHealth : MonoBehaviour {
+	public float overallScore;
+
+	void Start () {
+	}
+
+	void Update () {
+		overallScore = Time.deltaTime;
+	}
+	void OnGui(){
+		GUI.Box(new Rect(20,15,300,50),"Score: "+overallScore.ToString("f0"));
+	}
+}
