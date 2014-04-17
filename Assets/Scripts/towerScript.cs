5106b90c2d1e4118f192880a1d2ea2e6087f803d
Alma Sanchez <as0820a@american.edu>
2014-04-17 17:02:24 -0400

    "tower script changes"

--- Assets/Scripts/towerScript.cs
+++ Assets/Scripts/towerScript.cs
@@ -7,7 +7,6 @@ public class towerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
 	}
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "takeDamage") {
@@ -16,13 +15,19 @@ public class towerScript : MonoBehaviour {
 		}
 
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if(currentHealth <= 0){
 			currentHealth = 0;
 			Destroy(gameObject);
 		}
 	}
 	void OnGUI(){
-		GUI.Box(new Rect(150,15,130,25),"Tower 1 health: "+currentHealth.ToString("f0"));
-	}	
+			//if(gameobject.name == "tower1"){}
+			GUI.Box (new Rect (150, 15, 130, 25), "Tower 1 health: " + currentHealth.ToString ("f0"));
+			//if(gameObject.name == "tower2"{
+			GUI.Box (new Rect (350, 15, 130, 25), "Tower 2 health: " + currentHealth.ToString ("f0"));
+
+			//if(gameObject.name == "tower3"{}
+			GUI.Box(new Rect(550,15,130,25),"Tower 3 health: "+currentHealth.ToString("f0"));
+	}
 }
